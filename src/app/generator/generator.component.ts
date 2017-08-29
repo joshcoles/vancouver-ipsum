c68cde3522ebe519cffccc9476319644e2ab1e15
Josh Coles <josh@forgeandsmith.com>
2017-08-29 14:15:21 -0700

    Combined two functions and added comments in generator.component.ts.

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './generator.component.html',
   styleUrls: ['./generator.component.css']
 })
+
 export class GeneratorComponent implements OnInit {
   vancouverWords = [
     { word: 'seawall' },
@@ -37,52 +38,58 @@ export class GeneratorComponent implements OnInit {
     { word: 'I just need to be close to the mountains' },
     { word: 'west coast best coast' },
     { word: 'namaste' },
-    { word: '#blesst' },
     { word: 'sedins' },
     { word: 'new brunch spot on main street' },
-
   ];
- displayWords = false;
- paragraph = '';
- paragraphs = []
- instructions = 'How many paragraphs of vancouver ipsum do you want?'
- generateText = 'Generate';
- submitted = false;
- paragraphNumber = 1;
 
-  onSubmit() {
-    this.submitted = true;
-  }
+  displayWords = false;
 
+   // Each paragraph is generated uniquely and pushed into 'paragraphs' array
+   paragraph = '';
+   paragraphs = []
+
+
+  // Sets default paragraph number. This changes any time
+  // the user changes the number, and is not tied to the
+  // actual 'submit';
+
+  paragraphNumber = 1;
+  instructions = 'How many paragraphs of vancouver ipsum do you want?'
+  generateText = 'Generate';
+
+
+  // TODO error handling for if user types in a letter
+  // input comes in as string, so this turns it to number
   onUpdateParagraphNumber(event: any){
     this.paragraphNumber = parseInt(event.srcElement.value);
-    console.log(this.paragraphNumber);
   }
 
-  generateParagraph() {
-    let newParagraph = '';
-    for (let i = 0; i < this.vancouverWords.length; i++) {
-      let index = Math.floor(this.vancouverWords.length * Math.random());
-      if (i != this.vancouverWords.length - 1) {
-        newParagraph += this.vancouverWords[index].word + " ";
-      } else {
-        newParagraph += this.vancouverWords[index].word;
-      }
-    }
-    newParagraph += ".";
-    newParagraph = newParagraph.charAt(0).toUpperCase() + newParagraph.slice(1);
-    this.paragraphs.push(newParagraph);
-    console.log(this.paragraphs);
-    console.log("paragraphNumber: " + this.paragraphNumber);
-  }
 
+  // TODO remove duplicate words from same paragarph
+  // This function generates a new paragraph for each
+  // number in 'paragraphNumber', pushes it into 'paragraphs',
+  // and displays it
   onGenerate() {
-    for (var i = 0; i < this.paragraphNumber; i++) {
-      this.generateParagraph();
-      console.log("I'm looping: " + i);
-    }
+    let newParagraph = "";
     this.displayWords = true;
-    console.log(this.displayWords);
+
+    // Checks if it's the last loop. If not, add a space
+    // after word
+    for (var i = 0; i < this.paragraphNumber; i++) {
+      for (let i = 0; i < this.vancouverWords.length; i++) {
+        let index = Math.floor(this.vancouverWords.length * Math.random());
+        if (i != this.vancouverWords.length - 1) {
+          newParagraph += this.vancouverWords[index].word + " ";
+        } else {
+          newParagraph += this.vancouverWords[index].word;
+        }
+      }
+
+      // Capitalizes first letter + add period at end
+      newParagraph += ".";
+      newParagraph = newParagraph.charAt(0).toUpperCase() + newParagraph.slice(1);
+      this.paragraphs.push(newParagraph);
+    }
   }
 
   constructor() { }
